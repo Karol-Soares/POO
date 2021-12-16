a5b35eb0584c9c16b0312fc1eb7ed165a1f0db39
Karolina Araújo Soares <karolina.soares@SUMUP.LOCAL>
2021-12-16 00:47:30 -0300

    Incluindo pt 3, ex2, teste colaborador POO

---
+++ testecolaborador.java
@@ -0,0 +1,14 @@
+package poo;
+
+public class testecolaborador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		colaborador1 karol = new colaborador1 ("Karol","Rua da Marola, 872","44444444444",999999999, 20, 123, 25000,15);
+		
+		karol.imprimirInfo();
+		karol.calcularSalario();
+	}
+
+}
