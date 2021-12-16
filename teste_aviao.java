a30d0457a515da4dd55d850ddf0001a0fcde60ee
Karolina Araújo Soares <karolina.soares@SUMUP.LOCAL>
2021-12-16 00:50:41 -0300

    Incluindo pt 2, ex 4, teste aviao POO

---
+++ teste_aviao.java
@@ -0,0 +1,12 @@
+package poo;
+
+public class teste_aviao {
+	
+	public static void main(String[] args)
+	{
+		aviao aviao1 = new aviao ("Tam","médio",2020);
+		aviao1.imprimirInfo();
+				
+	}
+
+}
